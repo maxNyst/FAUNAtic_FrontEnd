b1f75d4cd7c3c127c2c73e914ff512b65ee42297
Max Nyström <max.k.nystrom@gmail.com>
2021-05-06 11:23:24 +0200

    first commit

---
+++ android/app/src/main/java/com/faunatic/faunatic_front_end/MainActivity.java
@@ -0,0 +1,6 @@
+package com.faunatic.faunatic_front_end;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:faunatic_front_end/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Faunatic the fabulous',
+      // Here is the color theme and text themes.
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.green.shade300,
+        primaryColor: Colors.green,
+        primarySwatch: Colors.green,
+        accentColor: Colors.orange
+      ),
+      home: Login(),
+    );
+  }
+}
+
+
