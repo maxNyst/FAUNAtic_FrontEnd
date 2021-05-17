436321179afe7655d84beef3ed218e325ada03fd
Max Nyström <max.k.nystrom@gmail.com>
2021-05-17 15:05:32 +0200

    added lecture plan page visuals

--- lib/Screens/Lecture/lecture_screen.dart
+++ lib/Screens/Lecture/lecture_screen.dart
@@ -5,10 +5,79 @@ class LecturesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: Text('Lectures'),),
-      body: Container(
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text('Save'),
+      ),
+      appBar: AppBar(
+        title: Text('Plan lecture'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.image_outlined,
+                size: 150,
+              ),
+              Text(
+                'Plan your excursion',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  height: size.height * 0.4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Excursion name',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 12),
+                            fillColor: Colors.white38,
+                            filled: true),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Place',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Structured moments',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Student tasks',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
