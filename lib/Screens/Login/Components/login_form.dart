c55668c213d8e8d0e31774b336b7c45ce8aaf48a
Max Nyström <max.k.nystrom@gmail.com>
2021-05-17 10:44:35 +0200

    fixed jumping input

--- lib/Screens/Login/Components/login_form.dart
+++ lib/Screens/Login/Components/login_form.dart
@@ -5,82 +5,90 @@ import 'package:provider/provider.dart';
 
 import '../../../authentication_service.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
+  LoginForm({Key key}) : super(key: key);
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  LoginForm({Key key}) : super(key: key);
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(25.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: _emailController,
-              validator: (email) {
-                if (email.contains('@') && email.contains('.'))
-                  return null;
-                else
-                  return 'Not a valid email.';
-              },
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Email"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 20.0,
-              ),
-              child: TextFormField(
-                controller: _passwordController,
-                validator: _passwordValidator,
-                obscureText: true,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _emailController,
+                validator: (email) {
+                  if (email.contains('@') && email.contains('.'))
+                    return null;
+                  else
+                    return 'Not a valid email.';
+                },
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
+                    border: OutlineInputBorder(), labelText: "Email"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 20.0,
+                ),
+                child: TextFormField(
+                  controller: _passwordController,
+                  validator: _passwordValidator,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 45),
-            ElevatedButton(
-              child: Text(
-                'Sign in',
+              SizedBox(height: 45),
+              ElevatedButton(
+                child: Text(
+                  'Sign in',
+                ),
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    String email = _emailController.text.trim();
+                    String password = _passwordController.text.trim();
+                    context.read<AuthenticationService>().signIn(
+                          email: email,
+                          password: password,
+                        );
+                  }
+                },
               ),
-              onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  String email = _emailController.text.trim();
-                  String password = _passwordController.text.trim();
-                  context.read<AuthenticationService>().signIn(
-                        email: email,
-                        password: password,
-                      );
-                }
-              },
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return SignupScreen();
-                    },
-                  ),
-                );
-              },
-              child: Text(
-                "Don't have an account? Sign up here",
-              ),
-            )
-          ],
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SignupScreen();
+                      },
+                    ),
+                  );
+                },
+                child: Text(
+                  "Don't have an account? Sign up here",
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
