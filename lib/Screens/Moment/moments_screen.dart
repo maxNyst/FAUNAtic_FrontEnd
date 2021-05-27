4c1e30ff8e9e6eb854bf84d62b4a592c4a68a5b0
Max Nyström <max.k.nystrom@gmail.com>
2021-05-27 13:48:19 +0200

    listview in momentsscreen able to add text

--- lib/Screens/Moment/moments_screen.dart
+++ lib/Screens/Moment/moments_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-class MomentsScreen extends StatelessWidget {
-  const MomentsScreen({Key key}) : super(key: key);
+class MomentsScreen extends StatefulWidget {
+  MomentsScreen({Key key}) : super(key: key);
+
+  @override
+  _MomentsScreenState createState() => _MomentsScreenState();
+}
+
+class _MomentsScreenState extends State<MomentsScreen> {
+  var _visibleFAB = true;
+  var list = [];
+  final TextEditingController eCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,49 +22,30 @@ class MomentsScreen extends StatelessWidget {
         title: Text('Hem'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Planera innehåll',
-                style: TextStyle(color: Colors.black, fontSize: 25),
-              )),
-          Divider(
-            thickness: 1,
-            color: Colors.grey,
-            indent: 10,
-            endIndent: 200,
-          ),
-          Container(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxHeight: 250),
-              child: TextField(
-                maxLines: null,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(5.0),
-                      ),
-                    ),
-                    hintText: 'Text...',
-                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
-                    fillColor: Colors.grey.shade100,
-                    filled: true),
-              ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    color: Colors.orangeAccent,
+                    width: size.width,
+                    height: 20,
+                    child: Text(list[index]),
+                  ),
+                );
+              },
             ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: CustomSpeedDial(),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: _buildSpeedDial(_visibleFAB),
       bottomNavigationBar: BottomAppBar(
         child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.max,
+          // mainAxisAlignment: MainAxisAlignment.start,
           children: [
             IconButton(
               onPressed: () {},
@@ -70,48 +60,70 @@ class MomentsScreen extends StatelessWidget {
       ),
     );
   }
-}
 
-class CustomSpeedDial extends StatelessWidget {
-  const CustomSpeedDial({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SpeedDial(
-      animatedIcon: AnimatedIcons.add_event,
-      animatedIconTheme: IconThemeData(size: 28.0, color: Colors.white),
-      backgroundColor: Theme.of(context).accentColor,
-      visible: true,
-      curve: Curves.bounceInOut,
-      children: [
-        SpeedDialChild(
-          child: Icon(Icons.location_on, color: Colors.white),
-          backgroundColor: Colors.green,
-          onTap: () => print('Pressed Read Later'),
-          label: 'Plats',
-          labelStyle:
-              TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
-          labelBackgroundColor: Colors.black,
-        ),
-        SpeedDialChild(
-          child: Icon(Icons.yard, color: Colors.white),
-          backgroundColor: Colors.green,
-          onTap: () => print('Pressed Write'),
-          label: 'Art',
-          labelStyle:
-              TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
-          labelBackgroundColor: Colors.black,
-        ),
-        SpeedDialChild(
-          child: Icon(Icons.subtitles, color: Colors.white),
-          backgroundColor: Colors.green,
-          onTap: () {},
-          label: 'Notering',
-          labelStyle:
-              TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
-          labelBackgroundColor: Colors.black,
-        ),
-      ],
+  Builder _buildSpeedDial(bool visible) {
+    return Builder(
+      builder: (BuildContext context) {
+        return SpeedDial(
+          animatedIcon: AnimatedIcons.add_event,
+          animatedIconTheme: IconThemeData(size: 28.0, color: Colors.white),
+          backgroundColor: Theme.of(context).accentColor,
+          visible: visible,
+          curve: Curves.bounceInOut,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.location_on, color: Colors.white),
+              backgroundColor: Colors.green,
+              onTap: () => print('Pressed Read Later'),
+              label: 'Plats',
+              labelStyle:
+                  TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+              labelBackgroundColor: Colors.black,
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.yard, color: Colors.white),
+              backgroundColor: Colors.green,
+              onTap: () => print('Pressed Write'),
+              label: 'Art',
+              labelStyle:
+                  TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+              labelBackgroundColor: Colors.black,
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.subtitles, color: Colors.white),
+              backgroundColor: Colors.green,
+              onTap: () {
+                setState(() {
+                  _visibleFAB = false;
+                });
+                var modalResponse = showModalBottomSheet(
+                  context: context,
+                  builder: (context) => Container(
+                    height: 200,
+                    child: TextField(
+                      controller: eCtrl,
+                      onSubmitted: (text) {
+                        eCtrl.clear();
+                        setState(() {
+                          list.add(text);
+                        });
+                        print(list.toString());
+                      },
+                    ),
+                  ),
+                );
+                modalResponse.whenComplete(() => setState(() {
+                      _visibleFAB = true;
+                    }));
+              },
+              label: 'Notering',
+              labelStyle:
+                  TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+              labelBackgroundColor: Colors.black,
+            ),
+          ],
+        );
+      },
     );
   }
 }
