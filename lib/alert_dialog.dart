a8e74c3a87b1802bdb3eac1088707b789dfcb6c0
MorrisRep <morris.reponen1@gmail.com>
2021-05-30 14:42:12 +0200

    test

--- lib/alert_dialog.dart
+++ lib/alert_dialog.dart
@@ -2,11 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FaunaticAlert extends StatelessWidget {
-  //final Function onPressed;
 
+  //test
   const FaunaticAlert({Key key}) : super(key: key);
-
-
 
 @override
 Widget build(BuildContext context) {
